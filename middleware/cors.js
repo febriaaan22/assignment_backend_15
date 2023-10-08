fbd1aab38c045b042caf7bff6d0d6389fe1e3d2f
Ireng Febrian <131000271+febriaan22@users.noreply.github.com>
2023-10-08 19:50:35 +0700

    origin edit

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,9 +1,9 @@
 const Xmark = {
-        origin: ['*', 'https://assignment-week-15-fe-x.netlify.app'],
+        origin: ['https://assignment-week-15-fe-x.netlify.app'],
         methods: ['GET', 'POST']
 }
 const Ymark = {
-        origin: ['https://assignment-week-15-fe-y.netlify.app','*'],
+        origin: ['https://assignment-week-15-fe-y.netlify.app'],
         methods: ['GET', 'POST', 'PUT', 'DELETE']
 }
 
