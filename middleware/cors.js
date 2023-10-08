09bed83ff9364e2def0aedcfa544b955e0bd9105
Ireng Febrian <131000271+febriaan22@users.noreply.github.com>
2023-10-08 18:15:16 +0700

    Penambahan Link

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,10 +1,10 @@
 const Xmark = {
-        origin: ['http://localhost:5173', 'https://main--unrivaled-haupia-d0d46b.netlify.app/'],
+        origin: ['http://localhost:5173', 'https://assignment-week-15-fe-x.netlify.app'],
         methods: ['GET', 'POST']
 }
 const Ymark = {
-        origin: ['https://main--unrivaled-haupia-d0d46b.netlify.app/','http://localhost:5173'],
+        origin: ['https://assignment-week-15-fe-y.netlify.app','http://localhost:5173'],
         methods: ['GET', 'POST', 'PUT', 'DELETE']
-    }
+}
 
 module.exports = { Xmark, Ymark }
