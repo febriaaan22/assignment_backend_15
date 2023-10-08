92a617dab1c4134657778eec6de1411c53e81c58
Ireng Febrian <131000271+febriaan22@users.noreply.github.com>
2023-10-08 20:00:54 +0700

    revise

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express()
 
 applyMiddleware(app);
 
-app.use('/user', routesBackend)
+app.use('/', routesBackend)
 
 const requestIdMiddleware = (req, res, next) => {
     if (req.headers['x-request-id']) {
