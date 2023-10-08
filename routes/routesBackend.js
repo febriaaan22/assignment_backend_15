e95c6e865e123005f832b170e3c479a0442ce1bc
Ireng Febrian <131000271+febriaan22@users.noreply.github.com>
2023-10-08 18:26:09 +0700

    Command

--- routes/routesBackend.js
+++ routes/routesBackend.js
@@ -6,13 +6,13 @@ const cors = require('cors')
 
 const routesBackend = Router()
 
-routesBackend.get('/get', cors(Xmark), getAllBackend)
-routesBackend.post('/post', cors(Xmark), postBackend)
-routesBackend.options('/get', cors(Xmark))
-routesBackend.options('/get', cors(Ymark))
-routesBackend.options('/delete', cors(Ymark))
-routesBackend.put('/put', cors(Ymark), putBackend)
-routesBackend.delete('/delete', cors(Ymark), deleteBackend)
+routesBackend.get('/get', getAllBackend)
+routesBackend.post('/post', postBackend)
+// routesBackend.options('/get')
+// routesBackend.options('/get')
+// routesBackend.options('/delete')
+routesBackend.put('/put', putBackend)
+routesBackend.delete('/delete', deleteBackend)
 
 
 module.exports = routesBackend
