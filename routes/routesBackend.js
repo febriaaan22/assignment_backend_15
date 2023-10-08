d31d76013c81dd6312ca73ffb3831d97872ec87b
Ireng Febrian <131000271+febriaan22@users.noreply.github.com>
2023-10-08 18:52:27 +0700

    Add

--- routes/routesBackend.js
+++ routes/routesBackend.js
@@ -1,18 +1,18 @@
 const { Router } = require('express')
 const { getAllBackend, postBackend, putBackend, deleteBackend } = require('../controller/controllerBackend')
-// const { Xmark, Ymark } = require('../middleware/cors')
+const { Xmark, Ymark } = require('../middleware/cors')
 const cors = require('cors')
 
 
 const routesBackend = Router()
 
-routesBackend.get('/get', getAllBackend)
-routesBackend.post('/post', postBackend)
-// routesBackend.options('/get')
-// routesBackend.options('/get')
-// routesBackend.options('/delete')
-routesBackend.put('/put', putBackend)
-routesBackend.delete('/delete', deleteBackend)
+routesBackend.get('/get', cors(Xmark), getAllBackend)
+routesBackend.post('/post', cors(Xmark), postBackend)
+routesBackend.options('/get', cors(Ymark))
+routesBackend.options('/get', cors(Xmark))
+routesBackend.options('/delete', cors(Ymark))
+routesBackend.put('/put', cors(Ymark), putBackend)
+routesBackend.delete('/delete', cors(Ymark), deleteBackend)
 
 
 module.exports = routesBackend
